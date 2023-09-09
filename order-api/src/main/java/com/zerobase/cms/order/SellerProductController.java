94120b3efc1a3ae7236e16f910e949ba2236b07b
postrel63 <postrel63@naver.com>
2023-09-09 23:48:17 +0900

    feat : [009-PRODUCT-ADD] ProductItem 추가 기능

--- order-api/src/main/java/com/zerobase/cms/order/SellerProductController.java
+++ order-api/src/main/java/com/zerobase/cms/order/SellerProductController.java
@@ -1,7 +1,10 @@
 package com.zerobase.cms.order;
 
+import com.zerobase.cms.order.Service.ProductItemService;
 import com.zerobase.cms.order.Service.ProductService;
+import com.zerobase.cms.order.domain.model.Product;
 import com.zerobase.cms.order.domain.product.AddProductForm;
+import com.zerobase.cms.order.domain.product.AddProductItemForm;
 import com.zerobase.cms.order.domain.product.ProductDto;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +18,7 @@ public class SellerProductController {
 
     private final ProductService productService;
     private final JwtAuthenticationProvider provider;
-
+    private  final ProductItemService productItemService;
 
     @PostMapping
     public ResponseEntity<ProductDto> addProduct(@RequestHeader(name = "X-AUTH-TOKEN") String token,
@@ -23,6 +26,14 @@ public class SellerProductController {
 
 
         return ResponseEntity.ok(ProductDto.from(productService.addProduct(provider.getUserVo(token).getId(),form)));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> addProductItem(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+                                                  @RequestBody AddProductItemForm form){
+
+
+        return ResponseEntity.ok(ProductDto.from(productItemService.addProductItem(provider.getUserVo(token).getId(),form)));
 
 
     }
--- order-api/src/main/java/com/zerobase/cms/order/Service/ProductItemService.java
+++ order-api/src/main/java/com/zerobase/cms/order/Service/ProductItemService.java
@@ -1,9 +1,16 @@
 package com.zerobase.cms.order.Service;
 
+import com.zerobase.cms.order.domain.model.Product;
+import com.zerobase.cms.order.domain.model.ProductItem;
+import com.zerobase.cms.order.domain.product.AddProductItemForm;
 import com.zerobase.cms.order.domain.repository.ProductItemRepository;
 import com.zerobase.cms.order.domain.repository.ProductRepository;
+import com.zerobase.cms.order.exception.CustomException;
+import com.zerobase.cms.order.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +22,19 @@ public class ProductItemService {
     private final ProductItemRepository productItemRepository;
 
 
+    public Product addProductItem(Long sellerId, AddProductItemForm form){
+        Product product = productRepository.findBySellerIdAndId(sellerId, form.getProductId())
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_PRODUCT));
+        if (product.getProductItems().stream()
+                .anyMatch(item -> item.getName().equals(form.getName()))){
+            throw new CustomException(ErrorCode.SAME_ITEM_NAME);
+        }
+
+        ProductItem productItem = ProductItem.of(sellerId, form);
+        product.getProductItems().add(productItem);
+
+        return product;
+
+    }
 
 }
