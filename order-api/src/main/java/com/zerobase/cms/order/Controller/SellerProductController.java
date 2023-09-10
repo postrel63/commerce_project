4205d470bc83e12fe38382b5fbe24636aa713233
postrel63 <postrel63@naver.com>
2023-09-11 02:06:48 +0900

    feat : [011-PRODUCT-DELETE] Product, productItem delete

--- order-api/src/main/java/com/zerobase/cms/order/Controller/SellerProductController.java
+++ order-api/src/main/java/com/zerobase/cms/order/Controller/SellerProductController.java
@@ -45,5 +45,20 @@ public class SellerProductController {
     }
 
 
+    @DeleteMapping
+    public ResponseEntity<Void> deleteProduct(@RequestHeader(name = "X-AUTH-TOKEN") String token, @RequestParam Long id){
+        productService.deleteProduct(provider.getUserVo(token).getId(),id);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/productItem")
+    public ResponseEntity<Void> deleteProductItem(@RequestHeader(name = "X-AUTH-TOKEN") String token, @RequestParam Long id){
+        productItemService.deleteProductItem(provider.getUserVo(token).getId(),id);
+
+        return ResponseEntity.ok().build();
+    }
+
+
 }
 
